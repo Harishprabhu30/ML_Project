93e2ecd4e3fa611358b557fc3c2a1b8b8fa59589
Harish Prabhu <harishprabhu3007@gmail.com>
2023-11-06 01:13:34 +0530

    Requirements Added

--- hh.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-# Print the current sys.path
-print(sys.path)
-
-# Remove a path from sys.path
-if '/path/to/custom/directory' in sys.path:
-    sys.path.remove('/path/to/custom/directory')
